b94017d879cd131d0d02c30f9df37129a3c7dfd9
Ulzahk <ulzahk@gmail.com>
2021-04-05 07:50:14 -0500

    [UPDATED] added fixed categories

--- src/components/CategoriesList/index.js
+++ src/components/CategoriesList/index.js
@@ -1,11 +1,51 @@
-import React from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { Category } from '../Category'
-import { categories } from '../../../api/db.json'
+import { categories as mockCategories } from '../../../api/db.json'
 import { List, Item } from './styles'
 
 export const CategoriesList = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState(mockCategories)
+  const [showFixed, setShowFixed] = useState(false)
+
+  // useEffect(() => {
+  //   window.fetch('https://practica-react-avanzado-2ebifnw4j-ulzahk.vercel.app/categories')
+  //     .then(res => res.json())
+  //     .then(response => {
+  //       setCategories(response)
+  //     })
+  // }, [])
+
+  // useEffect(() => {
+  //   let ignore = false
+
+  //   async function fetchData () {
+  //     const req = await window.fetch(
+  //       `https://petgram-app-server-ln46kf2xq.now.sh/categories`,
+  //     );
+  //     const res = await req.json()
+  //     if (!ignore) setCategories(res)
+  //   }
+
+  //   fetchData()
+  //   return () => {
+  //     ignore = true
+  //   }
+  // }, [])
+
+  useEffect(() => {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 180
+      showFixed !== newShowFixed &&
+      setShowFixed(newShowFixed)
+    }
+
+    document.addEventListener('scroll', onScroll)
+
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map(
           (category) =>
@@ -15,5 +55,12 @@ export const CategoriesList = () => {
         )
       }
     </List>
+  )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
   )
 }
