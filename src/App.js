1fcea02f361ca4165a0f1480d5b34961eba566d4
Ulzahk <ulzahk@gmail.com>
2021-04-04 19:58:00 -0500

    [CREATED] PhotoCard and PhotoCardsList components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { CategoriesList } from './components/CategoriesList/index'
 import { GlobalStyle } from './GlobalStyles'
+import { PhotoCardsList } from './components/PhotoCardsList'
 
 export const App = () => {
   return (
     <div>
       <GlobalStyle />
       <CategoriesList />
+      <PhotoCardsList />
     </div>
   )
 }
