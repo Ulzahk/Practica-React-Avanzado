dc300000f0ed1135faa54341060c1cdd0128573f
Ulzahk <ulzahk@gmail.com>
2021-04-18 12:10:51 -0500

    [UPDATED] added public and privated pages

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { GlobalStyle } from './components/styles/GlobalStyles'
 import { Logo } from './components/Logo'
+import { NavBar } from './components/NavBar'
+
 import { Home } from './pages/Home'
 import { Detail } from './pages/Details'
-import { NavBar } from './components/NavBar'
+import { Favs } from './pages/Favs'
+import { User } from './pages/User'
+import { NotRegisterUser } from './pages/NotRegisterUser'
+
+// const UserLogged = ({ children }) => {
+//   return children({ isAuth: false })
+// }
+
+const isAuth = false
 
 export const App = () => {
   return (
@@ -16,6 +26,17 @@ export const App = () => {
         <Route exact path='/' component={Home} />
         <Route path='/pet/:categoryId' component={Home} />
         <Route path='/detail/:detailId' component={Detail} />
+        {
+          isAuth
+            ? <>
+                <Route exact path='/favs' component={Favs} />
+                <Route exact path='/user' component={User} />
+              </>
+            : <>
+                <Route exact path='/favs' component={NotRegisterUser} />
+                <Route exact path='/user' component={NotRegisterUser} />
+              </>
+        }
       </Switch>
       <NavBar />
     </BrowserRouter>
